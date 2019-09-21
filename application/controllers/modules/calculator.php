347957bc01222be588fa0975908a4d04cbe3f8d8
= <=>
2019-09-22 02:15:44 +0600

    Module Calculator has been added

--- application/controllers/modules/calculator.php
+++ application/controllers/modules/calculator.php
@@ -4,8 +4,105 @@
 
     class Calculator extends Controller
     {
+        // Срок выделения средств
+        public $duration;
+        // Стоимость объекта
+        public $objPrice;
+        // Первоначальный взнос в %
+        public $initFeePercent;
+        // Первоначальный взнос
+        public $initFee;
+        // Вступительный взнос
+        public $entranceFee;
+        // Членский взнос
+        public $membershipFee;
+        // Итого членский взнос
+        public $totalMembershipFee;
+        // Первоначальный остаток паквого взноса
+        public $initShareRemain;
+        // Ежемесячный паевой взнос
+        public $monthlyShareFee;
+        // Ежемесячный платеж
+        public $monthlyPayment;
+        // Итого переплата
+        public $totalOverPayment;
+        // Сообщения об ошибках
+        public $error_msg = [
+            'duration' => '',
+            'obj_price' => '',
+            'init_fee' => '',
+        ];
+
+        // Месячный Расчетный Показатель МРП
+        private $mrp = 2525;
+        // Вступительный взнос в % (7%)
+        private $entranceFeePercent = 0.07;
+        // Минимальный срок выделения средств
+        private $durationMin = 1;
+        // Максимальный срок выделения средств
+        private $durationMax = 180;
+        // Минимальная стоимость объекта
+        private $objPriceMin = 3000000;
+        // Мнимальный процент перваначального взноса
+        private $initFeeMin = 28;
+
+        public function __construct()
+        {
+            parent::__construct();
+
+            if($this->calcFormValidate()){
+                $this->entranceFee = $this->objPrice * $this->entranceFeePercent;
+                $this->initFee = ($this->objPrice * $this->initFeePercent) / 100;
+                $this->initShareRemain = $this->objPrice - $this->initFee;
+                $this->monthlyShareFee = $this->initShareRemain / $this->duration;
+                $this->membershipFee = $this->mrp * 2;
+                $this->monthlyPayment = $this->membershipFee + $this->monthlyShareFee;
+                $this->totalMembershipFee = $this->membershipFee * $this->duration;
+                $this->totalOverPayment = $this->entranceFee + $this->totalMembershipFee;
+            }
+        }
+
         public function index()
         {
-            return $this->load->view('modules/calculator');
+            $data = array();
+            $data['duration'] = $this->duration;
+            $data['obj_price'] = $this->objPrice;
+            $data['init_fee_percent'] = $this->initFeePercent;
+            $data['init_fee'] = number_format($this->initFee, 0, '', ' ');
+            $data['entrance_fee'] = number_format($this->entranceFee, 0, '', ' ');
+            $data['membership_fee'] = number_format($this->membershipFee, 0, '', ' ');
+            $data['init_share_remain'] = $this->initShareRemain;
+            $data['monthly_share_fee'] = $this->monthlyShareFee;
+            $data['monthly_payment'] = number_format(ceil($this->monthlyPayment), 0, '', ' ');
+            $data['total_membership_fee'] = number_format($this->totalMembershipFee, 0, '', ' ');
+            $data['total_overpayment'] = number_format($this->totalOverPayment, 0, '', ' ');
+
+            $data['duration_msg'] = $this->error_msg['duration'];
+            $data['obj_price_msg'] = $this->error_msg['obj_price'];
+            $data['init_fee_msg'] = $this->error_msg['init_fee'];
+
+            return $this->load->view('modules/calculator', $data);
+        }
+
+        public function calcFormValidate()
+        {
+            if(
+                $this->request->has('duration', 'post') &&
+                $this->request->has('object-price', 'post') &&
+                $this->request->has('initial-fee', 'post')
+            ){
+                if((int)$this->request->post['duration'] >= $this->durationMin && (int)$this->request->post['duration'] <= $this->durationMax){ $this->duration = (int)$this->request->post['duration']; }
+                else{ $this->error_msg['duration'] = 'Срок выделения средств должен быть от 1 до 180 месяцев'; }
+
+                if((int)$this->request->post['object-price'] >= $this->objPriceMin){ $this->objPrice = (int)$this->request->post['object-price']; }
+                else{ $this->error_msg['obj_price'] = 'Стоимость объекта не должен быть меньше 3 000 000 тг'; }
+
+                if((int)$this->request->post['initial-fee'] >= $this->initFeeMin){ $this->initFeePercent = $this->request->post['initial-fee']; }
+                else{ $this->error_msg['init_fee'] = 'Первоначальный взнос не должен быть меньше 28 %'; }
+
+                if(!empty($this->duration && !empty($this->objPrice) && !empty($this->initFeePercent))){ return true; }
+                return false;
+            }
+            return false;
         }
     }
--- application/views/modules/calculator.php
+++ application/views/modules/calculator.php
@@ -3,22 +3,27 @@
         <h3 class="row-title title-center">Онлайн калькулятор</h3>
         <div class="row d-flex justify-content-center">
             <div class="calc-module col-lg-6 col-md-8 col-sm-10 col-xs-12">
-                <form action="" class="calc">
+                <form method="post" class="calc">
                     <div class="form-group">
                         <label for="duration">Срок выделения средств <span>(мес)</span></label>
-                        <input id="duration" type="text" name="duration" placeholder="от 1 до 180">
+                        <i class="calc-message"><?php if(isset($duration_msg)) echo $duration_msg; ?></i>
+                        <input id="duration" type="number" name="duration" value="<?php if(isset($duration)) echo $duration; ?>" placeholder="от 1 до 180" required>
                     </div>
                     <div class="form-group">
                         <label for="object-price">Стоимость объекта <span>(тг)</span></label>
-                        <input id="object-price" type="text" name="object-price" placeholder="от 3 000 000">
+                        <i class="calc-message"><?php if(isset($obj_price_msg)) echo $obj_price_msg; ?></i>
+                        <input id="object-price" type="number" name="object-price" value="<?php if(isset($obj_price)) echo $obj_price; ?>" placeholder="от 3 000 000" required>
                     </div>
                     <div class="form-group">
-                        <label for="start-payment">Первоначальный взнос <span>(%)</span></label>
-                        <input id="start-payment" type="text" name="start-payment" placeholder="от 28">
+                        <label for="initial-fee">Первоначальный взнос <span>(%)</span></label>
+                        <i class="calc-message"><?php if(isset($init_fee_msg)) echo $init_fee_msg; ?></i>
+                        <input id="initial-fee" type="number" name="initial-fee" value="<?php if(isset($init_fee_percent)) echo $init_fee_percent; ?>" placeholder="от 28" required>
                     </div>
                     <div class="form-froup">
                         <p class="form-group-title">Переплата за весь срок <span>(тг)</span></p>
-                        <p class="calc-result"></p>
+                        <p class="calc-result">
+                            <b><?php if(isset($total_overpayment)) echo $total_overpayment . ' тг'; ?></b>
+                        </p>
                     </div>
                     <input class="gen-btn main-btn" type="submit" value="Расчитать">
                 </form>
@@ -28,6 +33,7 @@
 </section>
 <section class="calculator-table">
     <div class="container-fluid">
+        <h3 class="row-title title-center">График платежей</h3>
         <div class="row">
             <div class="calc-table col-lg-12">
                 <table border="1">
@@ -43,58 +49,39 @@
                             <th>Ежемесячный платеж</th>
                         </tr>
                     </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>5 000 000</td>
-                        <td>350 000</td>
-                        <td>1 500 000</td>
-                        <td>58 333.3</td>
-                        <td>5 050</td>
-                        <td>3 441 666.6</td>
-                        <td>63 383.3</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>58 333.3</td>
-                        <td>5 050</td>
-                        <td>3 383 333.3</td>
-                        <td>63 383.3</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>58 333.3</td>
-                        <td>5 050</td>
-                        <td>3 325 000</td>
-                        <td>63 383.3</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>58 333.3</td>
-                        <td>5 050</td>
-                        <td>3 266 666.6</td>
-                        <td>63 383.3</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td>58 333.3</td>
-                        <td>5 050</td>
-                        <td>3 208 333.3</td>
-                        <td>63 383.3</td>
-                    </tr>
-                    </tbody>
+                    <?php if(isset($duration) && isset($obj_price) && isset($init_fee_percent)): ?>
+                        <tbody>
+                        <?php for($i = 1; $i <= $duration; $i++): ?>
+                            <?php $i%12 == 0 ? $class = 'calc-year': $class = ''; ?>
+                            <tr class="<?php echo $class; ?>">
+                                <td><?php echo $i; ?></td>
+                                <?php if($i == 1): ?>
+                                    <td><?php echo number_format($obj_price, 0, '', ' '); ?></td>
+                                    <td><?php echo $entrance_fee ?></td>
+                                    <td><?php echo $init_fee ?></td>
+                                <?php else: ?>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                <?php endif; ?>
+                                <td><?php echo number_format($monthly_share_fee, 0, '', ' '); ?></td>
+                                <td><?php echo $membership_fee; ?></td>
+                                <td><?php echo number_format(ceil($init_share_remain - ($monthly_share_fee * $i)), 0, '', ' '); ?></td>
+                                <td><?php echo $monthly_payment; ?></td>
+                            </tr>
+                        <?php endfor; ?>
+                        <tr>
+                            <td></td>
+                            <th>Итого переплата</th>
+                            <td><?php echo $entrance_fee?></td>
+                            <td></td>
+                            <td></td>
+                            <td><?php echo $total_membership_fee; ?></td>
+                            <td></td>
+                            <th><?php echo $total_overpayment; ?></th>
+                        </tr>
+                        </tbody>
+                    <?php endif; ?>
                 </table>
             </div>
         </div>
