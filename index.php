a9ce2992b9ec917cec1c88a7a329e2f31a6b89fa
= <=>
2019-10-09 12:28:54 +0600

    some configs has been changed

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-	require_once('config.php');
+	if(file_exists('config.php')){ require_once('config.php'); }
+	else if(file_exists('config_local.php')){ require_once('config_local.php'); }
+
 	require_once(LIB_PATH . 'devtools.php');
 	require_once(LIB_PATH . 'autoload.php');
 
